08cd3be5c2af53d8eede4e0780118d9098877b29
Amer <bdzamer@gmail.com>
2024-06-08 21:55:00 +0200

    Forgot to add a dot in the img file path  so I added it

--- Project3/src/Components/Content.jsx
+++ Project3/src/Components/Content.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { Grid, Typography, Card, CardContent } from "@mui/material"
+import React from "react";
+import { Grid, Typography, Card, CardContent } from "@mui/material";
 
-import Title from "./Title"
-import Paragraph from "./Paragraph"
+import Title from "./Title";
+import Paragraph from "./Paragraph";
 
 const Content = () => {
   return (
@@ -15,7 +15,7 @@ const Content = () => {
         justifyContent: "center",
         alignItems: "center",
         py: 10,
-        px: 2
+        px: 2,
       }}
     >
       <Grid item xs={12} sm={12} md={5} component="section" textAlign={"center"}>
@@ -28,18 +28,24 @@ const Content = () => {
           sx={{
             fontWeight: "400",
             paddingTop: 1,
-            color: "#333"
+            color: "#333",
           }}
         >
           Property facilities
         </Typography>
 
-        <Paragraph text={"QuickRecipeFinder is a simple and efficient web application designed to help you quickly find recipes based on ingredients or recipe names. This lightweight project leverages an API to deliver relevant recipes right to your screen with minimal effort. Whether you have a specific dish in mind or just a few ingredients you want to use, QuickRecipeFinder will provide you with a list of recipes to choose from. The streamlined interface ensures a hassle-free experience, focusing on easy search functionality without any additional features. Perfect for those who need quick inspiration for their next meal!"} maxWidth={"100%"} mx={0} />
+        <Paragraph
+          text={
+            "QuickRecipeFinder is a simple and efficient web application designed to help you quickly find recipes based on ingredients or recipe names. This lightweight project leverages an API to deliver relevant recipes right to your screen with minimal effort. Whether you have a specific dish in mind or just a few ingredients you want to use, QuickRecipeFinder will provide you with a list of recipes to choose from. The streamlined interface ensures a hassle-free experience, focusing on easy search functionality without any additional features. Perfect for those who need quick inspiration for their next meal!"
+          }
+          maxWidth={"100%"}
+          mx={0}
+        />
 
-        <img style={{ width: "100%", height: "auto" }} src="/src/assets\Gordon-Ramsay-PNG-Cutout.png" />
+        <img style={{ width: "100%", height: "auto" }} src="./src/assets\Gordon-Ramsay-PNG-Cutout.png" />
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
